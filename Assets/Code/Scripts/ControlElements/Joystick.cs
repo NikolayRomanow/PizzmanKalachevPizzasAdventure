0ec4160b8b8d20513bee976e68fdf10f61f09dfa
Nikita <system.program.code@gmail.com>
2021-04-05 19:47:50 +0500

    Added stove mechanik

--- Assets/Code/Scripts/ControlElements/Joystick.cs
+++ Assets/Code/Scripts/ControlElements/Joystick.cs
@@ -65,11 +65,6 @@ namespace Code.Scripts.ControlElements
                         ridger.position = _originRidgerPosition;
                         //joystickUI.SetActive(false);
                 }
-
-                else
-                {
-
-                }
             }
         }
 
